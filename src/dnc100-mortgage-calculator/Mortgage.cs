ffdb83e93da906f48dc82f91beb538fc26369a48
hankfrank <henryfrank1@yahoo.com>
2020-03-19 16:22:09 -0700

    all tests but calculate working

--- src/dnc100-mortgage-calculator/Mortgage.cs
+++ src/dnc100-mortgage-calculator/Mortgage.cs
@@ -6,34 +6,74 @@ namespace dnc100_mortgage_calculator
 {
     public class Mortgage
     {
-        public Mortgage(double principle, double interest, int term, int period)
-        {
+    private int period;
+    private int term;
+    private double interest;
+    private double principle;
 
+    public Mortgage(double mPrinciple, double mInterest, int mTerm, int mPeriod)
+        {
+            period = mPeriod;
+            term = mTerm;
+            interest = mInterest;
+            principle = mPrinciple;
+            
         }
 
         public double Calculate()
         {
-            return 0;
+            int months = period * term;
+            double r = interest / 1200;
+            double top = r * Math.Pow((1 + r), months);
+            double bottom = Math.Pow((1 + r), months) - 1;
+            double payment = (principle * (top / bottom));
+
+            string str = payment.ToString("0.00");
+            double pay = Convert.ToDouble(str);
+
+            return pay;
+            
         }
 
         public double MonthlyInterestRate(double interest, int period)
         {
-            return 0;
+
+            double percent = interest / 100;
+            double monthlyRate = percent / period;
+              
+            return monthlyRate;
         }
 
         public int NumberOfPayments(int term, int period)
         {
-            return 0;
+            return period * term;
         }
 
         public double CompoundedInterestRate(double monthlyInterestRate, int numberOfPayments)
         {
-            return 0;
+            double final = Math.Pow((1 + monthlyInterestRate), numberOfPayments);
+            return final;
         }
 
         public double InterestQuotient(double monthlyInterestRate, double compoundedInterestRate, int numberOfPayments)
         {
-            return 0;
+
+            double iQ = (monthlyInterestRate * numberOfPayments) * (1 - compoundedInterestRate);
+
+            return Math.Abs(iQ);
         }
     }
 }
+
+//public static double MortgageCalculator(double balance, double rate, int term, int period)
+//{
+//  int months = term * 12;
+//  double r = rate / 1200;
+//  double top = r * Math.Pow((1 + r), months);
+//  double bottom = Math.Pow((1 + r), months) - 1;
+//  double payment = (balance * (top / bottom));
+
+//  string str = payment.ToString("0.00");
+//  double pay = Convert.ToDouble(str);
+//  return pay;
+//}
--- src/dnc100-mortgage-calculator/Program.cs
+++ src/dnc100-mortgage-calculator/Program.cs
@@ -9,16 +9,48 @@ namespace dnc100_mortgage_calculator
             Mortgage mortgage;
             double principal, interestRate, monthlyPayment;
             int term, period;
+
             // Use .WriteLine to greet the user
+            Console.WriteLine("Whats up bro? I hear you're interested in Mortgage Rates, super cool.");
 
             // Use a mix of WriteLine and ReadLine to obtain the mortgage attributes (Making sure to typecast)
+            Console.WriteLine("Input the Principal Balance: ");
+            principal = Double.Parse(Console.ReadLine());
 
-            // Create a new Mortgage with the given attributes;
+            Console.WriteLine("Input the Interest Rate: ");
+            interestRate = Double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Input the Term: (# of years)");
+            term = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the Period (# of payments per year): ");
+            period = Int32.Parse(Console.ReadLine());
+
+            // Create a new Mortgage with the given attributes
+            mortgage = new Mortgage(principal, interestRate, term, period);
 
             // Use the Mortgage functions to calculate the monthly payment
 
-            // Use WriteLine to output the monthly payment
+            monthlyPayment = mortgage.Calculate();
 
-        }
+            int months = mortgage.NumberOfPayments(term, period);
+            double monthlyRate = mortgage.MonthlyInterestRate(interestRate, period);
+            double compoundInterest = mortgage.CompoundedInterestRate(monthlyRate, months);
+            double iQ = mortgage.InterestQuotient(monthlyRate, compoundInterest, months);
+
+            // Use WriteLine to output the monthly payment
+            Console.WriteLine($"principal: {principal}, interestRate: {interestRate}");
+            Console.WriteLine($"term: {term}, period: {period}");
+            Console.WriteLine($" your # of monthly payments: {months}");
+            Console.WriteLine($"monthly interest rate: {monthlyRate}");
+            Console.WriteLine($"Compound interest: {compoundInterest}");
+            Console.WriteLine($"IQ : {iQ}");
+            Console.WriteLine($"monthly payment: {monthlyPayment}");
+
+            Console.WriteLine($"hit any key to finish...");
+            Console.ReadKey();
+
+
+    }
     }
 }
